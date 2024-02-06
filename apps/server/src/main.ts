dca14e68c265e64d64adbf473146e63def2122ce
Jeyhun Rahimli <rahimlijeyhun77@gmail.com>
2024-02-06 06:11:34 +0100

    update: edit api prefix

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.enableCors();
 
   app.useGlobalPipes(new ValidationPipe());
-  app.setGlobalPrefix('api/v1/');
+  app.setGlobalPrefix('api/');
 
   app.use(
     ['/api-doc'],
